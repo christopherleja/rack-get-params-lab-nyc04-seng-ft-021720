7194f49461134f340cc4d779622fa1da83bfa902
Christopher Leja <christopher.leja@gmail.com>
2020-03-07 16:25:53 +0000

    Automatically backed up by Learn

--- app/application.rb
+++ app/application.rb
@@ -31,7 +31,7 @@ class Application
         resp.write "added #{item}"
 
       else
-        resp.write "We couldn't find #{item}"
+        resp.write "We don't have that item"
       end
     else
       resp.write "Path Not Found"
